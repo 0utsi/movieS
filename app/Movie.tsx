3ba0422e2ca9d5a3a50a1bbe29ff16ceca6ef933
“Mateusz” <mateusz.ludyk@gmail.com>
2023-02-14 17:14:22 +0100

    start

---
+++ app/Movie.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Movie({ title, id, poster_path, release_date }) {
+	const image_path = "https://image/tmdb.org/t/p/original/";
+	return (
+		<div>
+			{/* <h1>{title}</h1>
+			<h2>{release_date}</h2>
+			<Link href={`/asd`}>
+				<Image src={image_path + poster_path} alt="asd" />
+			</Link> */}
+		</div>
+	);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
+import { Montserrat } from "@next/font/google";
+import Home from "./page";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+const montserrat = Montserrat({
+	weight: ["400", "700"],
+	subsets: ["latin"],
+	variable: "--font-montserrat",
+});
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<head />
+			<body className={`${montserrat.className}`}>{children}</body>
+		</html>
+	);
 }
